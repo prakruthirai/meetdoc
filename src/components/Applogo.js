1cc5df85d9596b93ba373718a624b3b3bfc7851a
psanusha <118505694+psanusha@users.noreply.github.com>
2024-04-30 20:57:42 +0530

    updates

--- src/components/Applogo.js
+++ src/components/Applogo.js
@@ -4,3 +4,4 @@ export default function Applogo() {
   
   return <img src={HOUSE} alt="Applogo" />
 }
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="header" >
       <div className="logo-container">
         <div className="col-md-2 d-block m-2 p-2 applogo">          
             <Applogo />
