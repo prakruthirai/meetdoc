1e11340164d4227acac2c078c64a9aa6e1167688
psanusha <118505694+psanusha@users.noreply.github.com>
2024-05-05 17:04:10 +0530

    updates

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,55 +2,48 @@ import React from 'react';
 import { Card, CardGroup } from 'react-bootstrap'; // Importing Card and CardGroup components from react-bootstrap
  // Importing Card and Button components from react-bootstrap
  import './aboutpage.css';
-
+ 
 const AboutPage = () => {
   return (
-    <div>
+    <div style={{ marginTop: '240px' }}>
       <div className="about-us-heading">
         <h1>About Us</h1>
       </div>
-      <CardGroup>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>Transcript</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in
-              to additional content. This content is a little bit longer.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>Summary</Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to
-              additional content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>MOM</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in
-              to additional content. This card has even longer content than the
-              first to show that equal height action.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-      </CardGroup>
+      
+      <div className="card-container">
+        <CardGroup>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Transcript</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in
+                to additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Summary</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>MOM</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in
+                to additional content. 
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+      </div>
     </div>
   );
 }
